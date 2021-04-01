162419e988736655349c564d9903c39ed02c1ebb
ykweb <ykweb@list.ru>
2021-04-01 16:36:50 +0300

    Add module to config kartik-v/yii2-grid

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -44,5 +44,15 @@ return [
             ],
         ],
     ],
+    'modules' => [
+        'gridview' =>  [
+            'class' => '\kartik\grid\Module'
+            // enter optional module parameters below - only if you need to
+            // use your own export download action or custom translation
+            // message source
+            // 'downloadAction' => 'gridview/export/download',
+            // 'i18n' => []
+        ]
+    ],
     'params' => $params,
 ];
