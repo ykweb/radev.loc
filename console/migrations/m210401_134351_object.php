811a94082564c3af167680d6ec72502905326f90
ykweb <ykweb@list.ru>
2021-04-01 16:45:08 +0300

    Create migration

---
+++ console/migrations/m210401_134351_object.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m210401_134351_object
+ */
+class m210401_134351_object extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m210401_134351_object cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m210401_134351_object cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
---
+++ console/migrations/m210401_134418_object_task.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m210401_134418_object_task
+ */
+class m210401_134418_object_task extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m210401_134418_object_task cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m210401_134418_object_task cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
